1bd9b91188d61a65359ed3c43c9caa68a748d921
Rahul Rathi <111693923+rahul0037a@users.noreply.github.com>
2023-10-15 13:02:35 +0530

    Add files via upload

---
+++ src/components/Contact.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
+
+function Contact() {
+  return (
+    <div className="text-center">
+      <h1>Contact & Social Profiles</h1>
+      <div className="row mb-3">
+        <a
+          href="https://www.linkedin.com/in/rahul-rathi-85ab42206/"
+          className="btn btn-outline-dark col"
+        >
+          <FontAwesomeIcon icon={faLinkedin} className="icons" />
+          LinkedIn
+        </a>
+      </div>
+      <div className="row mb-3">
+        <a
+          href="mailto:rathirahul1000@gmail.com"
+          className="btn btn-outline-dark col"
+        >
+          <FontAwesomeIcon icon={faEnvelope} className="icons" />
+          Gmail
+        </a>
+      </div>
+      <div className="row">
+        <a
+          href="https://github.com/rahul0037a"
+          className="btn btn-outline-dark col"
+        >
+          <FontAwesomeIcon icon={faGithub} className="icons" />
+          GitHub
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/components/ExperienceAndEducation.js
@@ -0,0 +1,93 @@
+function ExperienceAndEducation() {
+  return (
+    <div className="contain">
+      <h1>Experience And Education</h1>
+      <hr />
+      <table>
+        <tbody>
+          <tr>
+            <td>
+              <h5>
+                MERN Full Stack Internship Program (August 2023 - Present)
+              </h5>
+              Ethnus Codemithra
+            </td>
+          </tr>
+
+          
+          <hr />
+          <tr>
+            <td>
+              <br />
+              Engaged in social media content creation and poster design for
+              cross-platform distribution and assisted in creating worksheets
+              for students.
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+            <h5>ETHNUS INTERNSHIP FIRST TASKS</h5>
+              CSS AND HTML TASKS OF ETHNUS INTERNSHIP
+              <br />
+              <a
+                href="https://github.com/rahul0037a/ethnus-webdev"
+                className="noDecore"
+              >
+                GitHub Repository
+              </a>
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <h5>Bachelor of Technology (2021-Present)</h5>
+              Vellore Institute of Technology, Chennai
+              <br />
+              Current CGPA (for 5 semesters): 8.67
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <h5>
+                Senior Secondary Education (XII<sup>th</sup> Grade) : CBSE Board
+                (2020)
+              </h5>
+              Bhagat Public School, Kota
+              <br />
+              Percentage: 81%
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <h5>ETHNUS INTERNSHIP SECOND TASKS</h5>
+              BOOTSTRAP AND JS TASKS OF ETHNUS INTERNSHIP
+              <br />
+              <a
+                href="https://github.com/rahul0037a/ethnus-webdev-js-bootstrap"
+                className="noDecore"
+              >
+                GitHub Repository
+              </a>
+            </td>
+          </tr>
+          <hr />
+          <tr>
+            <td>
+              <h5>
+                Secondary Education (X<sup>th</sup> Grade) : CBSE Board (2019)
+              </h5>
+              ST. Vincent's Convent Sr. Sec. School, Balasore
+              <br />
+              Percentage: 87%
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default ExperienceAndEducation;
